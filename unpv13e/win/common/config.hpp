a30465de48cbf1de581bf8b1c9616b6531390000
hexu <hexu_1985@sina.com>
2019-05-07 16:36:31 +0800

    add tcpserv_poll and tcpserv_select of win

--- unpv13e/win/common/config.hpp
+++ unpv13e/win/common/config.hpp
@@ -5,6 +5,18 @@
 #define WSVERS MAKEWORD(2, 0)
 #endif
 
+#ifndef OPEN_MAX
+#define OPEN_MAX    1024
+#endif
+
+/* POSIX requires that an #include of <poll.h> DefinE INFTIM, but many                                                                                                 
+ * systems still DefinE it in <sys/stropts.h>.  We don't want to include
+ * all the STREAMS stuff if it's not needed, so we just DefinE INFTIM here.
+ * This is the standard value, but there's no guarantee it is -1. */
+#ifndef INFTIM
+#define INFTIM          (-1)    /* infinite poll timeout */
+#endif
+
 #ifndef MAXLINE
 #define	MAXLINE	4096
 #endif
@@ -16,5 +28,6 @@
 #ifndef SERV_PORT 
 #define SERV_PORT 9877
 #endif
+
 
 #endif
