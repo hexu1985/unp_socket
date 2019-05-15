04236c7cbb56f0b4e0254a91aa02509d40b3d14f
hexu <hexu@sensetime.com>
2019-05-15 15:25:42 +0800

    add tcp_connect and tcp_listen

---
+++ unpv13e/bsd/common/tcp_listen.hpp
@@ -0,0 +1,10 @@
+#ifndef UNP_TCP_LISTEN_INC
+#define UNP_TCP_LISTEN_INC
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int tcp_listen(const char *, const char *, socklen_t *);
+int Tcp_listen(const char *, const char *, socklen_t *);
+
+#endif
